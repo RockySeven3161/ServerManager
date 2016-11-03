822b68604520e0ad93368b8e11648682f9c6a52a
RockySeven3161 <babaei.alireza580@GMAIL.COM>
2016-11-03 10:46:44 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
-bot_token = "178560551:AAGZtcrVedRN64xUx08dCM7cuC4mtldT2h0"
+bot_token = "271916388:AAHwJMADavLw3qa4TNuItJeyGqMR0cDzU8k"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "1.7"
-sudo_name = "Engineer Shayan Ahmadi"
-sudo_id = 180910633
-sudo_user = "shayan_soft"
+sudo_name = "GraphicDesigner87654"
+sudo_id = 295395556
+sudo_user = "GraphicDesigner87654"
 nerkh = "5"
